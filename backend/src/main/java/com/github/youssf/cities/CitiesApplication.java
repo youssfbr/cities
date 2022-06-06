fc3deeeeefeed95b3d2560ebd12ca690b9554933
Alisson Youssf <youssfbr@gmail.com>
2022-06-06 14:49:25 -0300

    Backend - Profiles, rename

--- backend/src/main/java/com/github/youssf/cities/CitiesApplication.java
+++ backend/src/main/java/com/github/youssf/cities/CitiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CitiesApiApplication {
+public class CitiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CitiesApiApplication.class, args);
+		SpringApplication.run(CitiesApplication.class, args);
 	}
 
 }
